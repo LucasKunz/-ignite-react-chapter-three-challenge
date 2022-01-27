c1eff3ae0c5f00320fe58159e2b22be7f7ae3981
Lucas Kunz <lucaskunz1@gmail.com>
2022-01-26 22:28:39 -0300

    Created header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import styles from './header.module.scss';
+import Link from 'next/link';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <span>
+            <img src="/logo.svg" alt="logo" />
+            spacetraveling
+          </span>
+        </a>
+      </Link>
+    </header>
+  );
 }
